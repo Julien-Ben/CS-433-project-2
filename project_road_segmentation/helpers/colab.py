eaa04d14b6423daffdd2f69183cccf14771a575a
hugolepeytre <hugo.lepeytre@gmail.com>
2020-11-29 21:35:05 +0100

    Added comments to the Colab script

--- project_road_segmentation/helpers/colab.py
+++ project_road_segmentation/helpers/colab.py
@@ -17,3 +17,14 @@ def download_model(model_name, save_dir, files):
     os.system(zip_cmd)
     files.download('./{}.zip'.format(model_name))
 
+# Add these lines to the notebook to pull your branch on the Drive
+# if COLAB:
+#     from helpers.colab import mount_and_pull
+#     from google.colab import drive
+#     mount_and_pull(BRANCH_NAME, drive, os)
+#
+# And these lines after saving your model to download it on your local system
+# if COLAB:
+#     from helpers.colab import download_model
+#     from google.colab import files
+#     download_model(MODEL_NAME, model_chosen['save_dir'], files)
