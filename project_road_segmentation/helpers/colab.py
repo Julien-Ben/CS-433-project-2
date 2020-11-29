2922193f06480f4af7fb710a72af85c64b6ac14e
hugolepeytre <hugo.lepeytre@gmail.com>
2020-11-29 21:09:38 +0100

    test4

--- project_road_segmentation/helpers/colab.py
+++ project_road_segmentation/helpers/colab.py
@@ -4,8 +4,8 @@ def mount_and_pull(branch_name, drive, os):
     os.chdir(drive_path)
     mgc = get_ipython().magic
     os.system('git fetch')
-    os.system('git checkout {}'.format(branch_name))
     os.system('git reset --hard')
+    os.system('git checkout {}'.format(branch_name))
     os.system('git pull')
     os.system('git branch')
     mgc('cd project_road_segmentation')
