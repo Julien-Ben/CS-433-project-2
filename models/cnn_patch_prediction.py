d5eae70113d3ab10905c6d5d199d00287ae17f23
hugolepeytre <hugo.lepeytre@gmail.com>
2020-12-15 11:51:58 +0100

    Refactoring the models

--- models/cnn_patch_prediction.py
+++ models/cnn_patch_prediction.py
@@ -8,12 +8,13 @@ from tensorflow.keras.layers import concatenate, add
 
 IMAGE_SIZE = 400
 
-def cnn_ben(img_size = IMAGE_SIZE):
-    kernel_size = (3,3)
-    first_kernel_size = (5,5)
-    dropout=0.2
-    leakyrate=0.05
-    model = tf.keras.models.Sequential([# Note the input shape is the desired size of the image 200x200 with 3 bytes color
+
+def cnn_ben(img_size=IMAGE_SIZE):
+    kernel_size = (3, 3)
+    first_kernel_size = (5, 5)
+    dropout = 0.2
+    leakyrate = 0.05
+    model = tf.keras.models.Sequential([  # Note the input shape is the desired size of the image 200x200 with 3 bytes color
     # This is the first convolution
     tf.keras.layers.Conv2D(64, first_kernel_size, activation=LeakyReLU(alpha=leakyrate), input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), padding='same'),
     tf.keras.layers.Conv2D(128, kernel_size, activation=LeakyReLU(alpha=leakyrate), padding='same'),
